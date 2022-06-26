73513c531b309686704fdcc823ce050eba667350
hiro <ubuntu@ip-172-31-25-43.ec2.internal>
2022-06-26 08:58:26 +0000

    表示部分追加＆controller調整

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
 gem 'bootsnap',   '1.10.3', require: false
+gem 'natto'
+gem 'naive_bayes'
 
 group :development, :test do
   gem 'sqlite3', '1.4.1'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
 class HomeController < ApplicationController
+  
+  def split_word(text)
+    natto = Natto::MeCab.new
+    word_arr = Array.new
+    if text.present?
+      natto.parse(text) do |parsed_word|
+        word_arr << parsed_word.surface if parsed_word.feature.split(',')[0] == "名詞" # 名詞のみ抽出
+      end
+    else
+      return nil
+    end
+    word_arr
+  end
+
   def top
-    puts "aa"*60
-    puts params[:search]
+    @genre = nil
+    naive = NaiveBayes.load('machine_learning.rb')
+    test_data = split_word(params[:search]) # 名詞のみ配列で抽出
+    if test_data.present?
+      @genre = naive.classify(*test_data)
+    end
   end
 end
